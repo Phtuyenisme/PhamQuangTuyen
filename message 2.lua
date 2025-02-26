b717a6c9e0cfd056ced79cd38fd4ade96475a1c3
Phtuyenisme <tuyen2quang@gmail.com>
2025-02-26 22:33:25 +0700

    Update message 2.lua

--- message 2.lua
+++ message 2.lua
@@ -28,25 +28,27 @@ local Tab = Window:MakeTab({"Script Farm",
 Tab:AddButton({
 	Name = "W-azure Hub",
 	Callback = function()
-       loadstring(game:HttpGet("https://raw.githubusercontent.com/Yumiara/Overflow/refs/heads/main/Main.lua"))()
+       getgenv().Team = "Pirates"
+getgenv().AutoLoad = false --Will Load Script On Server Hop
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
   	end    
 })
 Tab:AddButton({
 	Name = "Redz Hub",
 	Callback = function()
-       loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/a5c3af437cd698d64379cf75cacb9281.lua"))()
+       loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))()
   	end    
 })
 Tab:AddButton({
 	Name = "Min Gaming",
 	Callback = function()
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/wpisstestfprg/Volcano/refs/heads/main/VolcanoLocal.lua"))()
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaCrack/Min/refs/heads/main/MinME"))()
   	end    
 })
 Tab:AddButton({
 	Name = "Trẩu Roblox",
 	Callback = function()
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/wpisstestfprg/Volcano/refs/heads/main/VolcanoLocal.lua"))()
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaCrack/TrauHub/refs/heads/main/TrauXt"))()
   	end    
 })
 Tab:AddButton({
@@ -58,49 +60,52 @@ Tab:AddButton({
 Tab:AddButton({
 	Name = "Mukuro Hub",
 	Callback = function()
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/wpisstestfprg/Volcano/refs/heads/main/VolcanoLocal.lua"))()
+     loadstring(game:HttpGet("https://auth.quartyz.com/scripts/Loader.lua"))()
   	end    
 })
 Tab:AddButton({
 	Name = "Alchemy Hub",
 	Callback = function()
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/wpisstestfprg/Volcano/refs/heads/main/VolcanoLocal.lua"))()
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/x2-Neptune/AlchemyHub/main/Main.lua"))({ "discord.gg/alchemyhub","x2neptune.tech/LoaderPromo",true,"BY:x2Neptune" })
   	end    
 })
 Tab:AddButton({
 	Name = "Raito Hub",
 	Callback = function()
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/wpisstestfprg/Volcano/refs/heads/main/VolcanoLocal.lua"))()
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/Efe0626/RaitoHub/refs/heads/main/Script"))()
   	end    
 })
 Tab:AddButton({
 	Name = "Speed Hub",
 	Callback = function()
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/wpisstestfprg/Volcano/refs/heads/main/VolcanoLocal.lua"))()
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua
   	end    
 })
 Tab:AddButton({
 	Name = "Teddy Hub",
 	Callback = function()
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/wpisstestfprg/Volcano/refs/heads/main/VolcanoLocal.lua"))()
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/skibiditoiletgojo/Haidepzai/refs/heads/main/TeddyHubv2"))()
   	end    
 })
 Tab:AddButton({
 	Name = "Xero Hub",
 	Callback = function()
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/wpisstestfprg/Volcano/refs/heads/main/VolcanoLocal.lua"))()
+     getgenv().Team = "Pirates"
+getgenv().Hide_Menu = false
+getgenv().Auto_Execute = false
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Xero2409/XeroHub/refs/heads/main/main.lua"))()
   	end    
 })
 Tab:AddButton({
 	Name = "AstralV1",
 	Callback = function()
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/wpisstestfprg/Volcano/refs/heads/main/VolcanoLocal.lua"))()
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/Overgustx2/Main/refs/heads/main/BloxFruits_25.html"))()
   	end    
 })
 Tab:AddButton({
 	Name = "QuanTum Hub",
 	Callback = function()
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/wpisstestfprg/Volcano/refs/heads/main/VolcanoLocal.lua"))()
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/Trustmenotcondom/QTONYX/refs/heads/main/QuantumOnyx.lua"))()
   	end    
 })
 Tab:AddButton({
