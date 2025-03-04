3ce15feff577565c4a54f37fb42548b792851b5d
Phtuyenisme <tuyen2quang@gmail.com>
2025-03-04 14:37:30 +0700

    Update ScriptTongHop.lua

--- ScriptTongHop.lua
+++ ScriptTongHop.lua
@@ -1,2 +1,3 @@
 --ScriptBY:PhtuyenzZ
-
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Script Tổng Hợp | Phtuyen⚡️", HidePremium = false, SaveConfig = true, ConfigFolder = "PhtuyenConfig"})
