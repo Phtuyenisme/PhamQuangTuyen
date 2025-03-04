3e4cb7e4ddbcf23904fa4e4e66c808047d2d2ff0
Phtuyenisme <tuyen2quang@gmail.com>
2025-03-04 19:09:53 +0700

    Update ScriptTongHop.lua

--- ScriptTongHop.lua
+++ ScriptTongHop.lua
@@ -2,6 +2,10 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Script Tổng Hợp[V3] | Phtuyen⚡️", HidePremium = false, IntroEnabled = false, IntroText = "BUỒI | LỒN", SaveConfig = true, ConfigFolder = "PhtuyenConfig"})
 
+for _, tween in pairs(TweenService:GetChildren()) do
+    tween:Destroy()
+end
+
 task.wait(1) -- Đợi 1 giây để UI load
 local MainUI = game.CoreGui:FindFirstChild("Orion") -- Tìm UI chính
 if MainUI then
