a7970212e14ce08292c0616cde8ce7d0e56621b5
Phtuyenisme <tuyen2quang@gmail.com>
2025-03-05 14:40:54 +0700

    Update ScriptTongHop.lua

--- ScriptTongHop.lua
+++ ScriptTongHop.lua
@@ -1,33 +1,6 @@
-clocal Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/Phtuyenisme/PhamQuangTuyen/refs/heads/main/message.lua")()
+local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
 
 local win = Flux:Window("PREVIEW", "Baseplate", Color3.fromRGB(255, 110, 48), Enum.KeyCode.LeftControl)
-local function printSizeAndPosition(uiElement)
-    print("Trước khi chuyển đổi:")
-    print(uiElement.Name .. " - Size: " .. tostring(uiElement.Size))
-    print(uiElement.Name .. " - Position: " .. tostring(uiElement.Position))
-end
-
-local function convertOffsetToScale(guiObject)
-    if guiObject.Parent and guiObject.Parent.AbsoluteSize then
-        local parentSize = guiObject.Parent.AbsoluteSize
-
-        printSizeAndPosition(guiObject) -- In ra trước khi đổi
-
-        guiObject.Size = UDim2.new(
-            guiObject.Size.X.Offset / parentSize.X, 0,
-            guiObject.Size.Y.Offset / parentSize.Y, 0
-        )
-        guiObject.Position = UDim2.new(
-            guiObject.Position.X.Offset / parentSize.X, 0,
-            guiObject.Position.Y.Offset / parentSize.Y, 0
-        )
-
-        print("Sau khi chuyển đổi:")
-        print(guiObject.Name .. " - Size: " .. tostring(guiObject.Size))
-        print(guiObject.Name .. " - Position: " .. tostring(guiObject.Position))
-    end
-end
-
 local tab = win:Tab("Tab 1", "http://www.roblox.com/asset/?id=6023426915")
 tab:Button("Kill all", "This function may not work sometimes and you can get banned.", function()
 Flux:Notification("Killed all players successfully!", "Alright")
