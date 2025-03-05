68fece3f1d34b9790adb32960a845c027fe613a2
Phtuyenisme <tuyen2quang@gmail.com>
2025-03-05 22:40:53 +0700

    Update ScriptTongHop.lua

--- ScriptTongHop.lua
+++ ScriptTongHop.lua
@@ -2717,7 +2717,7 @@ end
 end
 
 local Main = royxui:royxstart("Tổng Hợp[V3] | Phtuyenisme⚡️")
-royxui:NewNotification("Phtuyen", "TuongVyy", 4, "rbxassetid://112969263935543")
+royxui:NewNotification("Đã Chạy Script Thành Công!", "⛅TuongVyy", 0.5, "rbxassetid://112969263935543")
 
 local Tab = Main:royxtab("Tab 1")
 
