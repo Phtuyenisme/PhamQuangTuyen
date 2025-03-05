edb94d39fe31a827dd4cd9e397cdb28c6e9ca818
Phtuyenisme <tuyen2quang@gmail.com>
2025-03-05 12:42:38 +0700

    Update ScriptTongHop.lua

--- ScriptTongHop.lua
+++ ScriptTongHop.lua
@@ -1,82 +1,28 @@
- local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
-local Window = Fluent:CreateWindow({
-    Title = "name",
-    SubTitle = "by name",
-    TabWidth = 160,
-    Size = UDim2.new(0, 580, 0, 460),
-    Acrylic = true,
-    Theme = "Dark",
-    MinimizeKey = Enum.KeyCode.LeftControl
-})
+local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
 
-Window.Draggable = false
-Window.Position = UDim2.new(0.3, 0, 0.2, 0)
-
-local Tabs = {
-    Main = Window:AddTab({ Title = "Main", Icon = "rbxassetid://4483345998" })
-}
-
-Tabs.Main:AddSection("name")
-local hakiRunning = false
-
-local VirtualInputManager = game:GetService("VirtualInputManager")
-
-Tabs.Main:AddToggle({
-    Title = "name tab",
-    Description = "name",
-    Default = false,
-    Callback = function(value)
-        hakiRunning = value
-        if hakiRunning then
-            spawn(function()
-                while hakiRunning do
-                    VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.J, false, game)
-                    wait(0.1)
-                    VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.J, false, game)
-                    wait(5)
-                end
-            end)
-            Fluent:Notify({
-                Title = "name",
-                Content = "i dont know",
-                Duration = 3
-            })
-        else
-            Fluent:Notify({
-                Title = "name",
-                Content = "i dont know",
-                Duration = 3
-            })
-        end
-    end
-}):SetIcon("rbxassetid://4483345998")
-
-Tabs.Main:AddSection("Menu Control")
-
-local menuVisible = true
-
-Tabs.Main:AddButton({
-    Title = "Ẩn/Hiện Menu",
-    Description = "Ẩn hoặc hiện menu",
-    Callback = function()
-        if menuVisible then
-            Window:TweenSize(UDim2.new(0, 0, 0, 0), "Out", "Quad", 0.3, true)
-            menuVisible = false
-            Fluent:Notify({
-                Title = "name",
-                Content = "i dont know",
-                Duration = 3
-            })
-        else
-            Window:TweenSize(UDim2.new(0, 580, 0, 460), "Out", "Quad", 0.3, true)
-            menuVisible = true
-            Fluent:Notify({
-                Title = "????",
-                Content = "???",
-                Duration = 3
-            })
-        end
-    end
-})
-
-Window:SelectTab(1)
+local win = Flux:Window("PREVIEW", "Baseplate", Color3.fromRGB(255, 110, 48), Enum.KeyCode.LeftControl)
+local tab = win:Tab("Tab 1", "http://www.roblox.com/asset/?id=6023426915")
+tab:Button("Kill all", "This function may not work sometimes and you can get banned.", function()
+Flux:Notification("Killed all players successfully!", "Alright")
+end)
+tab:Label("This is just a label.")
+tab:Line()
+tab:Toggle("Auto-Farm Coins", "Automatically collects coins for you!", function(t)
+print(t)
+end)
+tab:Slider("Walkspeed", "Makes your faster.", 0, 100,16,function(t)
+print(t)
+end)
+tab:Dropdown("Part to aim at", {"Torso","Head","Penis"}, function(t)
+print(t)
+end)
+tab:Colorpicker("ESP Color", Color3.fromRGB(255,1,1), function(t)
+print(t)
+end)
+tab:Textbox("Gun Power", "This textbox changes your gun power, so you can kill everyone faster and easier.", true, function(t)
+print(t)
+end)
+tab:Bind("Kill Bind", Enum.KeyCode.Q, function()
+print("Killed a random person!")
+end)
+win:Tab("Tab 2", "http://www.roblox.com/asset/?id=6022668888")
