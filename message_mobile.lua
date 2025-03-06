7b2b84aa6fdca87d27c199f9030fdef5cb4937c7
Phtuyenisme <tuyen2quang@gmail.com>
2025-03-06 12:52:45 +0700

    Update message_mobile.lua

--- message_mobile.lua
+++ message_mobile.lua
@@ -2708,22 +2708,3 @@ end
 	end
 	return royxtabui
 end
-
-local Main = royxui:royxstart("Tổng Hợp[V3] | Phtuyenisme⚡️")
-royxui:NewNotification("Đã Chạy Script Thành Công!", "⛅TuongVyy", 1, "rbxassetid://112969263935543")
-
-local Tab = Main:royxtab("Tab 1")
-
-local Tab2 = Main:royxtab("Tab 2")
-
-local Page = Tab:royxpage("Auto Farm")
-
-Page:Label("Phtuyenisme")
-
-Page:Button("Button",function()
-
-end)
-
-Page:Button("Notification",function()
-	royxui:NewNotification("Đã Chạy Script Thành Công!", "⛅TuongVyy", 1, "rbxassetid://112969263935543")
-end)
