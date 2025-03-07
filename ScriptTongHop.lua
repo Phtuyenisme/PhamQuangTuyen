b0c4676502df9df26d786a5f860d1dcaa84cc704
Phtuyenisme <tuyen2quang@gmail.com>
2025-03-07 14:10:29 +0700

    Update ScriptTongHop.lua

--- ScriptTongHop.lua
+++ ScriptTongHop.lua
@@ -2876,15 +2876,34 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaCrack/Min/refs/hea
 end)
 
 PageTab3:Button("Auto Chest [TpBypass,only one island]",function()
-  _G.Settings = { 
-    ["Enable Farm"] = true, 
-    ["SnipeLegendaryItem"] = true, 
-    ["Choose Team"] = "Marines", 
-    ["White Screen"] = false, 
-    ["Reset Anti Detect"] = true 
-} 
+  getgenv().BloxFruits = {
+    ["Team"] = "Marines", -- Pirates/Marines
+    ["TweenSpeed"] = 350, -- Studs per second 380 no flag but kick
+    ["Fruit"] = {
+        ["FruitNotifier"] = false, -- Fruit notifier
+        ["AutoRandom"] = true, -- Auto random/store fruit
+        ["FruitSniper"] = {
+            ["Enabled"] = false, -- Auto buy fruit in normal shop
+            ["TargetFruits"] = {"Yeti-Yeti", "Dragon-Dragon", "Dough-Dough"},
+        },
+    },
+    ["Farm"] = {
+        ["Enabled"] = true, -- Farm chest & fruit
+        ["FastMethod"] = false, -- Faster but more risky
+        ["AutoHop"] = true, -- Auto hop after collect all
+        ["ItemHop"] = true, -- God's Chalice/Fist of Darkness
+    },
+    ["Webhook"] = {
+        ["Enabled"] = false,
+        ["URL"] = "Your Webhook Url",
+        ["UserId"] = "Id Discord",
+    },
+    ["BlackScreen"] = false, -- Black screen
+    ["FpsBoost"] = false, -- Boost FPS
+    ["AntiIdle"] = true, -- Anti AFK
+}
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xxhumggxx/SkullHub/refs/heads/main/ChestV2.lua"))()
+loadstring(game:HttpGet('https://skullhub.xyz/loader.lua'))()
 end)
 
 --4💗💗💗💗
